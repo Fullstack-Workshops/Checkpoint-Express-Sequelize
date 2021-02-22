769ada44662b0df71a817bc83b09b768cdad5cc1
azrielgoldman <goldmanazriel@yahoo.com>
2021-02-22 13:41:01 -0500

    Completed tasks model

--- models/express-models/todos.js
+++ models/express-models/todos.js
@@ -18,21 +18,36 @@ module.exports = {
   // ==== COMPLETE THE FOLLOWING (SEE `model.js` TEST SPEC) =====
   listPeople: function () {
     // returns an array of all people for whom tasks exist
+    return Object.keys(tasks);
   },
 
   add: function (name, task) {
     // saves a task for a given person
+    if (!task.complete) {
+      task.complete = false;
+    } else {
+      task.complete = true;
+    }
+    if (tasks[name]) {
+      tasks[name].push(task);
+    } else {
+      tasks[name] = [task];
+    }
+    return task;
   },
 
   list: function (name) {
     // returns tasks for specified person
+    return tasks[name];
   },
 
   complete: function (name, idx) {
     // marks a task complete
+    tasks[name][idx].complete = true;
   },
 
   remove: function (name, idx) {
     // removes a tasks
+    tasks[name].splice(idx, 1);
   },
 };
