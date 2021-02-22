57d27c9481a379b49e2db7c874479ce59209b3ab
azrielgoldman <goldmanazriel@yahoo.com>
2021-02-22 16:04:40 -0500

    Completed the Sequelize models Task and Owner

--- models/sequelize-models/index.js
+++ models/sequelize-models/index.js
@@ -33,7 +33,50 @@ const Owner = db.define('Owner', {
 Task.belongsTo(Owner);
 Owner.hasMany(Task);
 
+Task.clearCompleted = async function() {
+  return await this.destroy({
+    where: {
+      complete: true
+    }
+  })
+};
 
+Task.completeAll = async function() {
+  return await this.update(
+    { complete: true }, {
+    where: {
+      complete: false
+    }
+  })
+};
+
+Task.prototype.getTimeRemaining = function() {
+  return !this.due ? Infinity : this.due - new Date();
+};
+
+Task.prototype.isOverdue = function() {
+  let difference = this.due - new Date();
+  return (difference < 0 && this.complete === false);
+};
+
+Task.prototype.assignOwner = function(owner) {
+  return this.setOwner(owner)
+}
+
+Owner.getOwnersAndTasks = async function() {
+  return await this.findAll({ include: [Task] })
+}
+
+Owner.prototype.getIncompleteTasks = async function() {
+  let tasks = await this.getTasks();
+  return tasks.filter((task) => task.complete === false)
+}
+
+Owner.beforeDestroy((owner) => {
+  if (owner.name === 'Grace Hopper') {
+    throw Error('An owner cannot have the name Grace Hopper!');
+  }
+})
 //---------^^^---------  your code above  ---------^^^----------
 
 module.exports = {
