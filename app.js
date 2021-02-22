605ded246c3260d1f1aab00582fe98b3dcfb3247
azrielgoldman <goldmanazriel@yahoo.com>
2021-02-22 14:20:19 -0500

    Completed the express GET, PUT, POST and DELETE routes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 module.exports = app; // this line is only used to make testing easier.
 
 // remember to plug in your router and any other middleware you may need here (i.e. body parser, mounting any router-level middleware, etc.)
+app.use(express.json());
+app.use('/users', require('./routes/index'));
 
 app.use((err, req, res, next) => {
   res.sendStatus(err.status);
--- routes/index.js
+++ routes/index.js
@@ -4,3 +4,65 @@ const todos = require('../models/express-models/todos');
 module.exports = router;
 
 // write your routes here. Feel free to split into multiple files if you like.
+router.get('/', (req, res, next) => {
+  try {
+    let users = todos.listPeople();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+})
+
+router.get('/:name/tasks', (req, res, next) => {
+  try {
+    let user = todos.list(req.params.name);
+    if (!user) {
+      let err = Error('User does not exist');
+      err.status = 404;
+      throw err;
+    }
+    if (req.query.status === 'complete') {
+      let completed = user.filter((todo) => todo.complete === true)
+      res.json(completed);
+    } else if (req.query.status === 'active') {
+      let active = user.filter((todo) => todo.complete === false);
+      res.json(active);
+    } else {
+      res.json(user);
+    }
+  } catch (error) {
+    next(error);
+  }
+})
+
+router.post('/:name/tasks', (req, res, next) => {
+  try {
+    let added = todos.add(req.params.name, req.body);
+    if (!req.body.content) {
+      let err = Error('todo to add does not exist');
+      err.status = 400;
+      throw err;
+    }
+    res.status(201).json(added);
+  } catch (error) {
+    next(error);
+  }
+})
+
+router.put('/:name/tasks/:idx', (req, res, next) => {
+  try {
+    todos.complete(req.params.name, req.params.idx);
+    res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
+})
+
+router.delete('/:name/tasks/:idx', (req, res, next) => {
+  try {
+    todos.remove(req.params.name, req.params.idx);
+    res.sendStatus(204)
+  } catch (error) {
+    next(error);
+  }
+})
